f2b39e596add3e42ea8c5258ee4b80182c7a5286
Geerten-vB <s1141481@student.hsleiden.nl>
2023-12-08 08:52:58 +0100

    Created springboot project

---
+++ IPRWC-backendCode/src/main/java/com/example/iprwcbackendcode/IprwcBackendCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.iprwcbackendcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcBackendCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprwcBackendCodeApplication.class, args);
+	}
+
+}
---
+++ IPRWC-backendCode/src/test/java/com/example/iprwcbackendcode/IprwcBackendCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iprwcbackendcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcBackendCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
